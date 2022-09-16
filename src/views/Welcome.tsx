e91ccf8d8db7327f6cc69f389265827a624ded30
sherry <lemon19961007@sina.com>
2022-09-16 09:41:46 +0800

    添加四个欢迎页面图片切换的动画

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,11 @@
-import {defineComponent} from 'vue';
-import {RouterView} from 'vue-router';
+import {defineComponent, Transition, VNode} from 'vue';
+import {RouteLocationNormalizedLoaded, RouterView} from 'vue-router';
 import s from '../stylesheets/Welcome/Welcome.module.scss';
 import tree from '../assets/icons/tree.svg';
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
+    type Y = { Component: VNode, route: RouteLocationNormalizedLoaded };
     return () => (
       <div class={s.wrapper}>
         <header>
@@ -14,7 +15,17 @@ export const Welcome = defineComponent({
           </div>
         </header>
         <main>
-          <RouterView name="main"/>
+          <RouterView name="main">
+            {({Component: Content, route: R}: Y) =>
+              <Transition
+                enterFromClass={s.slideFadeEnterFrom}
+                enterActiveClass={s.slideFadeEnterActive}
+                leaveToClass={s.slideFadeLeaveTo}
+                leaveActiveClass={s.slideFadeLeaveActive}>
+                {Content}
+              </Transition>
+            }
+          </RouterView>
         </main>
         <footer>
           <RouterView name="footer"/>
