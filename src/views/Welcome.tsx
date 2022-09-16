f111b0c50276cabf0661330f61572dd6cfc6346c
sherry <lemon19961007@sina.com>
2022-09-16 08:25:07 +0800

    添加welcome的主题标志

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,12 +1,18 @@
 import {defineComponent} from 'vue';
 import {RouterView} from 'vue-router';
 import s from '../stylesheets/Welcome/Welcome.module.scss';
+import tree from '../assets/icons/tree.svg';
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
     return () => (
       <div class={s.wrapper}>
-        <header></header>
+        <header>
+          <div class={s.headerContent}>
+            <img src={tree} class={s.iconStyle}></img>
+            <span class={s.fontStyle}>森林记账</span>
+          </div>
+        </header>
         <main><RouterView/></main>
         <footer></footer>
       </div>
