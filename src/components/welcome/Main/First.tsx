964507454de262fb9afcb4c00e1d77f01140766e
sherry <lemon19961007@sina.com>
2022-09-16 13:41:39 +0800

    完成四张欢迎卡片的触屏滑动切换

--- src/components/welcome/Main/First.tsx
+++ src/components/welcome/Main/First.tsx
@@ -1,32 +1,20 @@
 import s from '../../../stylesheets/Welcome/WelcomeLayout.module.scss';
-import {defineComponent, FunctionalComponent, ref, watchEffect} from 'vue';
-import {useRouter} from 'vue-router';
-import {useSwipe} from '../../../hooks/useSwipe';
+import {FunctionalComponent} from 'vue';
 
-export const First = defineComponent({
-  setup() {
-    const div = ref<HTMLElement>();
-    const router = useRouter();
-    const {swiping, direction} = useSwipe(div, {
-      beforeStart: e => e.preventDefault()
-    });
-    watchEffect(() => {
-      if (swiping.value && direction.value === 'left') {
-        router.push('/welcome/2');
-      }
-    });
-    return () => (
-      <div class={s.wrapper}>
-        <div class={s.card} ref={div}>
-          <svg class={s.iconStyle}>
-            <use xlinkHref="#first"></use>
-          </svg>
-          ,
-          <div class={s.fontStyle}>
-            <div>这是一段话<br/>这里还有一段话</div>
-          </div>
+export const First: FunctionalComponent = () => {
+  return (
+    <div class={s.wrapper}>
+      <div class={s.card}>
+        <svg class={s.iconStyle}>
+          <use xlinkHref="#first"></use>
+        </svg>
+        ,
+        <div class={s.fontStyle}>
+          <div>这是一段话<br/>这里还有一段话</div>
         </div>
       </div>
-    );
-  }
-});
+    </div>
+  );
+};
+
+First.displayName = 'First';
--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -17,10 +17,10 @@ export const routes: RouteRecordRaw[] = [
     component: Welcome,
     children: [
       {path: '', redirect: '/welcome/1',},
-      {path: '1', components: {main: First, footer: FirstAction}},
-      {path: '2', components: {main: Second, footer: SecondAction}},
-      {path: '3', components: {main: Third, footer: ThirdAction}},
-      {path: '4', components: {main: Forth, footer: ForthAction}},
+      {path: '1', name: 'welcome1', components: {main: First, footer: FirstAction}},
+      {path: '2', name: 'welcome2', components: {main: Second, footer: SecondAction}},
+      {path: '3', name: 'welcome3', components: {main: Third, footer: ThirdAction}},
+      {path: '4', name: 'welcome4', components: {main: Forth, footer: ForthAction}},
     ]
   },
   {path: '/start', component: Start}
