caaebedfd6cdcfd3199628d22a5c7fca446343f8
sherry <lemon19961007@sina.com>
2022-09-16 12:28:54 +0800

    添加useSwipe hook,并为main添加useSwipe

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,16 @@
-import {defineComponent, Transition, VNode} from 'vue';
+import {defineComponent, ref, Transition, VNode, watchEffect} from 'vue';
 import {RouteLocationNormalizedLoaded, RouterView} from 'vue-router';
 import s from '../stylesheets/Welcome/Welcome.module.scss';
+import {useSwipe} from '../hooks/useSwipe';
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
     type Y = { Component: VNode, route: RouteLocationNormalizedLoaded };
+    const main = ref<HTMLElement | null>(null);
+    const {direction, swiping} = useSwipe(main);
+    watchEffect(() => {
+      console.log( direction.value);
+    });
     return () => (
       <div class={s.wrapper}>
         <header>
@@ -15,7 +21,7 @@ export const Welcome = defineComponent({
             <span class={s.fontStyle}>森林记账</span>
           </div>
         </header>
-        <main>
+        <main ref={main}>
           <RouterView name="main">
             {({Component: Content, route: R}: Y) =>
               <Transition
