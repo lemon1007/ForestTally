7bf91e0a7c6bde98f2b927d64bfce05d7e9aff24
sherry <lemon19961007@sina.com>
2022-09-16 13:19:06 +0800

    欢迎页面滑动切换路由

--- src/components/welcome/Main/First.tsx
+++ src/components/welcome/Main/First.tsx
@@ -1,28 +1,32 @@
 import s from '../../../stylesheets/Welcome/WelcomeLayout.module.scss';
-import {FunctionalComponent, ref, watchEffect} from 'vue';
+import {defineComponent, FunctionalComponent, ref, watchEffect} from 'vue';
 import {useRouter} from 'vue-router';
 import {useSwipe} from '../../../hooks/useSwipe';
 
-export const First: FunctionalComponent = () => {
-  const div = ref<HTMLElement>();
-  const router = useRouter();
-  const {swiping, direction} = useSwipe(div);
-  watchEffect(() => {
-    if (swiping.value && direction.value === 'left') {
-      router.push('/welcome/2');
-    }
-  });
-  return (
-    <div class={s.wrapper}>
-      <div class={s.card} ref={div}>
-        <svg class={s.iconStyle}>
-          <use xlinkHref="#first"></use>
-        </svg>
-        ,
-        <div class={s.fontStyle}>
-          <div>这是一段话<br/>这里还有一段话</div>
+export const First = defineComponent({
+  setup() {
+    const div = ref<HTMLElement>();
+    const router = useRouter();
+    const {swiping, direction} = useSwipe(div, {
+      beforeStart: e => e.preventDefault()
+    });
+    watchEffect(() => {
+      if (swiping.value && direction.value === 'left') {
+        router.push('/welcome/2');
+      }
+    });
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card} ref={div}>
+          <svg class={s.iconStyle}>
+            <use xlinkHref="#first"></use>
+          </svg>
+          ,
+          <div class={s.fontStyle}>
+            <div>这是一段话<br/>这里还有一段话</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+});
