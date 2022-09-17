4ba66e669d1d9e800f0883385fcc3a977d8fef13
sherry <lemon19961007@sina.com>
2022-09-17 18:07:40 +0800

    完成记一笔界面

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -12,23 +12,56 @@ export const InputPad = defineComponent({
   },
   setup: (props, context) => {
     const refDate = ref<Date>();
-    const now = Date();
+    const appendText = (n: number | string) => {
+      const nString = n.toString();
+      const dotIndex = refAmount.value.indexOf('.');
+      if (refAmount.value.length >= 13) {
+        return;
+      }
+      if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
+        return;
+      }
+      if (nString === '.') {
+        if (dotIndex >= 0) { // 已经有小数点了
+          return;
+        }
+      } else if (nString === '0') {
+        if (dotIndex === -1) { // 没有小数点
+          if (refAmount.value === '0') { // 没小数点，但是有0
+            return;
+          }
+        }
+      } else {
+        if (refAmount.value === '0') {
+          refAmount.value = '';
+        }
+      }
+      refAmount.value += n.toString();
+    };
     const buttons = [
-      {text: '1', onClick: () => {}},
-      {text: '2', onClick: () => {}},
-      {text: '3', onClick: () => {}},
-      {text: '清空', onClick: () => {}},
-      {text: '4', onClick: () => {}},
-      {text: '5', onClick: () => {}},
-      {text: '6', onClick: () => {}},
-      {text: '+', onClick: () => {}},
-      {text: '7', onClick: () => {}},
-      {text: '8', onClick: () => {}},
-      {text: '9', onClick: () => {}},
-      {text: '-', onClick: () => {}},
-      {text: '.', onClick: () => {}},
-      {text: '0', onClick: () => {}},
-      {text: '回删', onClick: () => {}},
+      {text: '1', onClick: () => {appendText(1);}},
+      {text: '2', onClick: () => {appendText(2);}},
+      {text: '3', onClick: () => {appendText(3);}},
+      {text: '清空', onClick: () => { refAmount.value = '0';}},
+      {text: '4', onClick: () => {appendText(4);}},
+      {text: '5', onClick: () => {appendText(5);}},
+      {text: '6', onClick: () => {appendText(6);}},
+      {text: '', onClick: () => {}},
+      {text: '7', onClick: () => {appendText(7);}},
+      {text: '8', onClick: () => {appendText(8);}},
+      {text: '9', onClick: () => {appendText(9);}},
+      {text: '', onClick: () => {}},
+      {text: '0', onClick: () => {appendText(0);}},
+      {text: '.', onClick: () => {appendText('.');}},
+      {
+        text: '回删', onClick: () => {
+          if (refAmount.value.length === 1) {
+            refAmount.value = '0';
+          } else {
+            refAmount.value = refAmount.value.slice(0, -1);
+          }
+        }
+      },
       {text: '提交', onClick: () => {}},
     ];
     const refDatePickerVisible = ref(false);
@@ -38,6 +71,8 @@ export const InputPad = defineComponent({
       refDate.value = date;
       hideDatePicker();
     };
+    const refAmount = ref('0');
+    const refNotes = ref('');
     return () => (<>
         <div class={s.showInfo}>
           <span class={s.createdAt}>
@@ -45,19 +80,17 @@ export const InputPad = defineComponent({
             <span class={s.date}>
               <span onClick={showDatePicker}>{time(refDate.value).format()}</span>
               <Popup position="bottom" v-model:show={refDatePickerVisible.value}>
-                <DatetimePicker value={refDate.value}
-                              type="date" title="请选择时间"
-                              onConfirm={setDate}
-                              onCancel={hideDatePicker}/>
+                <DatetimePicker value={refDate.value} type="date" title="请选择时间" onConfirm={setDate}
+                                onCancel={hideDatePicker}/>
               </Popup>
           </span>
           </span>
-          <span class={s.amount}>123456789</span>
+          <span class={s.amount}>{refAmount.value}</span>
         </div>
 
         <div class={s.notes}>
           <Icon name="test" class={s.notes_icon}></Icon>
-          <input placeholder="备注"></input>
+          <input placeholder="添加备注">{refNotes.value}</input>
         </div>
 
         <div class={s.buttons}>
