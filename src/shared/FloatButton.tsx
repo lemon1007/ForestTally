5bcab73aec6ec2e77a56a7851a2db178131f5335
sherry <lemon19961007@sina.com>
2022-09-17 09:54:44 +0800

    修复IconName允许为空的问题

--- src/shared/FloatButton.tsx
+++ src/shared/FloatButton.tsx
@@ -1,12 +1,18 @@
-import {defineComponent} from 'vue';
+import {defineComponent, PropType} from 'vue';
 import {Icon} from './Icon';
 import s from '../stylesheets/sharedStyle/FloatButton.module.scss';
 
 export const FloatButton = defineComponent({
+  props: {
+    IconName: {
+      type: String as PropType<string>,
+      required: true,
+    }
+  },
   setup: (props, context) => {
     return () => (
       <div class={s.floatButton}>
-        <Icon name="add" class={s.icon}></Icon>
+        <Icon name={props.IconName} class={s.icon}></Icon>
       </div>
     );
   }
