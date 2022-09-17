7b73298803a5c87415de7a78f8f7a57bf0b0c399
sherry <lemon19961007@sina.com>
2022-09-17 20:55:30 +0800

    wip:创建TagEdit.tsx

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,17 +1,12 @@
-import {defineComponent, PropType, reactive, toRaw} from 'vue';
+import {defineComponent, reactive} from 'vue';
 import {MainLayout} from '../../layouts/MainLayout';
 import {Button} from '../../shared/Button';
 import {Icon} from '../../shared/Icon';
-import s from '../../stylesheets/tag/TagCreate.module.scss';
+import s from '../../stylesheets/tag/Tag.module.scss';
 import {EmojiSelect} from '../../shared/EmojiSelect ';
 import {Rules, validate} from '../../shared/validate';
 
 export const TagCreate = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup: (props, context) => {
     const formData = reactive({
       name: '',
