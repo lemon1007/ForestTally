7017292e4c882aa5538a993eb0259a3d502f4384
sherry <lemon19961007@sina.com>
2022-09-17 11:50:59 +0800

    创建overlay组件

---
+++ src/shared/Overlay.tsx
@@ -0,0 +1,45 @@
+import {defineComponent, PropType} from 'vue';
+import s from '../stylesheets/sharedStyle/Overlay.module.scss';
+import {Icon} from './Icon';
+
+export const Overlay = defineComponent({
+  props: {
+    onClose: {
+      type: Function as PropType<() => void>
+    }
+  },
+  setup: (props, context) => {
+    const close = () => {
+      props.onClose?.();
+    };
+    return () => <>
+      <div class={s.mask} onClick={close}></div>
+      <div class={s.overlay}>
+        <section>
+          <h2>未登录用户</h2>
+          <p>点击登录</p>
+        </section>
+        <nav>
+          <ul>
+            <li>
+              <Icon name="test"></Icon>
+              <span>统计图表</span>
+            </li>
+            <li>
+              <Icon name="test"></Icon>
+              <span>导出数据</span>
+            </li>
+            <li>
+              <Icon name="test"></Icon>
+              <span>自定义分类</span>
+            </li>
+            <li>
+              <Icon name="test"></Icon>
+              <span>记账提醒</span>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>;
+  }
+});
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,31 +1,38 @@
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
 import {Button} from '../shared/Button';
 import {FloatButton} from '../shared/FloatButton';
 import {Center} from '../shared/Center';
 import {Icon} from '../shared/Icon';
 import {Navbar} from '../shared/Navbar';
 import s from '../stylesheets/StartPage.module.scss';
+import {Overlay} from '../shared/Overlay';
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
-    const onClick = () => {
-      console.log('hi');
+    const refOverlayVisible = ref(false);
+    const onClickMenu = () => {
+      refOverlayVisible.value = !refOverlayVisible.value;
+      console.log(refOverlayVisible.value);
     };
     return () => (
       <div>
         <Navbar>{
           {
-            default: '森林记账',
-            icon: <Icon name="menu" class={s.navIcon}/>
+            default: () => '森林记账',
+            icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu}/>
           }
         }</Navbar>
         <Center class={s.icon_wrapper}>
           <Icon name="tree" class={s.icon}></Icon>
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>开始记账</Button>
+          <Button class={s.button}>开始记账</Button>
         </div>
         <FloatButton IconName="add"/>
+        {
+          refOverlayVisible.value &&
+          <Overlay onClose={() => refOverlayVisible.value = false}/>
+        }
       </div>
     );
   }
