dee88e819ffbb41753a8eca1d3297f7fc7426f04
sherry <lemon19961007@sina.com>
2022-09-17 09:28:04 +0800

    封装Icon组件

---
+++ src/shared/Icon.tsx
@@ -0,0 +1,18 @@
+import {defineComponent, PropType} from 'vue';
+import s from '../stylesheets/sharedStyle/Icon.module.scss';
+
+export const Icon = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<string>,
+      required: true,
+    }
+  },
+  setup: (props, context) => {
+    return () => (
+      <svg class={s.icon}>
+        <use xlinkHref={'#' + props.name}></use>
+      </svg>
+    );
+  }
+});
